a6af70bbb4e76a2a5f986279bc5cad9e78fb6735
developerv2000 <developerv2000@gmail.com>
2024-04-24 15:37:12 +0500

    Fixing supervision page errors

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,6 +6,7 @@ use App\Models\Product;
 use App\Models\Research;
 use App\Models\Slide;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class MainController extends Controller
 {
